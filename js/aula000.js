3238fc41d2cedc3078a14550b90c37f4348aa7f7
Henrique87645 <henriquedominguez12@gmail.com>
2024-04-18 14:54:54 -0300

    18/04

--- js/aula000.js
+++ js/aula000.js
@@ -2,7 +2,7 @@ const readline = require('readline-sync');
 
 let tentativas = 0;
 const senhaCorreta = "123";
-let saldo = 1000; // Saldo inicial
+let saldo = 0; // Saldo inicial
 
 while (tentativas < 3) {
     const senha = readline.question("Digite a senha: ");
@@ -14,51 +14,53 @@ while (tentativas < 3) {
             console.log("2. Depósito");
             console.log("3. Empréstimo");
             console.log("4. Sair");
-            opcao = readline.questionInt("Escolha uma opção: ");
+            opcao = readline.questionInt("Escolha uma opcao: ");
             
-            if (opcao === 1 || opcao === 2 || opcao === 3 || opcao === 4) {
+            if (opcao < 1 || opcao > 4) {
+                console.log("Opcao incorreta");
                 break;
-            } else {
-                console.log("Opção incorreta");
             }
-        }
         
-        if (opcao === 1) {
-            const saque = parseFloat(readline.question("Digite o valor que deseja sacar: "));
-            if (saque <= 0 || saque > saldo) {
-                console.log("Valor inválido");
-            } else {
-                saldo -= saque;
-                console.log(`Saque de ${saque} realizado. Novo saldo: ${saldo}`);
+            if (opcao === 1) {
+                const saque = parseFloat(readline.question("Digite o valor que deseja sacar R$ "));
+                if (saque <= 0 || saque > saldo) {
+                    console.log("Valor inválido")
+                } else {
+                    saldo -= saque
+                    console.log(`Saque de R$ ${saque} realizado. Novo saldo bancario R$ ${saldo}`)
+                }
+
+            } else if (opcao === 2) {
+                const deposito = parseFloat(readline.question("Digite o valor que deseja depositar R$ "));
+                if (deposito <= 0) {
+                    console.log("Valor inválido");
+                } else {
+                    saldo += deposito;
+                    console.log(`Depósito de R$ ${deposito} realizado. Novo saldo bancario R$${saldo}`);
+                }
+
+            } else if (opcao === 3) {
+                const valorEmprestimo = parseFloat(readline.question("Digite o valor do emprestimo R$ "));
+                const parcelas = parseInt(readline.question("Digite a quantidade de parcelas: "));
+                const valorParcela = valorEmprestimo / parcelas;
+
+                if (valorEmprestimo <= saldo / 3) {
+                    saldo += valorEmprestimo;
+                    console.log(`Emprestimo aprovado, novo saldo de R$ ${saldo}. Novas parcelas de R$ ${valorParcela}`);
+                } else {
+                    console.log("Emprestimo reprovado");
+                }
+
+            } else if (opcao === 4) {
+                console.log("Encerrando o programa");
+                break;
             }
-        } else if (opcao === 2) {
-            const deposito = parseFloat(readline.question("Digite o valor que deseja depositar: "));
-            if (deposito <= 0) {
-                console.log("Valor inválido");
-            } else {
-                saldo += deposito;
-                console.log(`Depósito de ${deposito} realizado. Novo saldo: ${saldo}`);
-            }
-        } else if (opcao === 3) {
-            const valorEmprestimo = parseFloat(readline.question("Digite o valor do empréstimo: "));
-            const parcelas = parseInt(readline.question("Digite a quantidade de parcelas: "));
-            const valorParcela = parseFloat(readline.question("Digite o valor das parcelas: "));
-            if (valorEmprestimo <= saldo / 3) {
-                saldo += valorEmprestimo;
-                console.log("Empréstimo aprovado. Novo saldo: " + saldo);
-            } else {
-                console.log("Empréstimo reprovado");
-            }
-        } else if (opcao === 4) {
-            console.log("Encerrando o programa");
-            break;
         }
+        break;
     } else {
         console.log("Senha incorreta. Tente novamente.");
         tentativas++;
+        if(tentativas == 3)
+            break
     }
-}
-
-if (tentativas === 3) {
-    console.log("Número de tentativas excedida! Encerrando o sistema");
 }
