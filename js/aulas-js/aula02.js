d67e1dcaaea2f51130c2e79a7550688fc403ebfb
Henrique87645 <henriquedominguez12@gmail.com>
2024-05-13 11:03:43 -0300

    13/05

--- js/aulas-js/aula02.js
+++ js/aulas-js/aula02.js
@@ -4,9 +4,8 @@ let total = 0;
 
 console.log("Bem-vindo ao sistema de caixa do mercado.");
 
-while (true) 
-{
-    console.log("Opções:");
+while (true) {
+    console.log("opcao:");
     console.log("1) Passar compra");
     console.log("2) Sair");
 
@@ -15,36 +14,27 @@ while (true)
     if (opcao === '2') {
         console.log('Encerrando o programa.');
         break;
-    } else if (opcao === '1') 
-    {
-        let continuar = true;
-        while (continuar) 
-        {
+    } else if (opcao === '1') {
+        while (true) {
             let nome = readlineSync.question('Coloque o nome do produto: ');
-            let preco = parseFloat(readlineSync.question('Insira o preco do produto: '));
+            let preco = parseFloat(readlineSync.question('Insira o preco do produto R$: '));
             let quantidade = parseInt(readlineSync.question('Quantos produtos voce deseja?: '));
-
             total += preco * quantidade;
 
             let resposta = readlineSync.question('Deseja inserir outro produto? (S/N): ');
-            if (resposta.toLowerCase() === 'N') 
-            {
-                continuar = false;
-
-                resposta = readlineSync.question('Deseja finalizar a compra com desconto? (S/N): ');
-                if (resposta.toLowerCase() === 'S') 
-                {
-                    let desconto = parseFloat(readlineSync.question('Insira o valor do desconto: '));
+            if (resposta.toLowerCase() === 'n') {
+                let aplicarDesconto = readlineSync.question('Deseja finalizar a compra com desconto? (S/N): ');
+                if (aplicarDesconto.toLowerCase() == 's') {
+                    let desconto = parseFloat(readlineSync.question('Insira o valor do desconto R$: '));
                     total -= desconto;
-                    console.log('Valor total da compra com desconto:', total.toFixed(2));
-                } 
-                else
-                {
-                    console.log('Valor total da compra:', total.toFixed(2));
-                }
+                    } 
+                console.log('Valor total da compra R$:', total.toFixed(2));
+                break; // Encerra o loop 
             }
         }
+        break; // Encerra o programa após o usuário finalizar a compra
     } else {
         console.log('Opcao incorreta. Encerrando o programa.');
-    }break;
+        break;
+    }
 }
