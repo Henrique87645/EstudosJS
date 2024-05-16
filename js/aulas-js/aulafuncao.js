4551b98836ab4ac3b9e4284918cf4ccbcf6282f4
Henrique87645 <henriquedominguez12@gmail.com>
2024-05-16 11:59:29 -0300

    16/05

--- js/aulas-js/aulafuncao.js
+++ js/aulas-js/aulafuncao.js
@@ -1,4 +1,5 @@
-
+const readlineSync = require('readline-sync');
+let imparpar = readlineSync.question("Digite um numero e descubra se e par ou impar: ")
 function parimpar(b) {
     if (b%2==0){
         return 'Par'
@@ -6,24 +7,29 @@ function parimpar(b) {
         return 'Impar'
     }
 }
-console.log(parimpar(12))
+console.log(parimpar(imparpar))
 
-//----------------------------
+console.log('--------------------------------------------------------------------')
 
+let som1 = parseFloat(readlineSync.question('Digite um numero: '));
+let som2 = parseFloat(readlineSync.question('Digite outro numero: '));
 function soma(n1=0 , n2=0) {
     return n1 + n2
 }
-console.log(soma(2, 7))
+console.log(soma(som1, som2))
 
-//----------------------------
+console.log('--------------------------------------------------------------------')
 
+let mult1 = parseFloat(readlineSync.question('Digite um numero: '));
+let mult2 = parseFloat(readlineSync.question('Digite o numero que desaja mutiplicar: '));
 let v = function(x) {
-    return x*2
+    return x*mult2
 }
-console.log(v(5))
+console.log(v(mult1))
 
-//----------------------------
+console.log('--------------------------------------------------------------------')
 
+let fator = parseFloat(readlineSync.question('Qual o numero que deseja descobrir o fatorial?: '))
 function fatorial(a) {
     let fat = 1 
     for(let c = a; c > 1; c--){
@@ -31,10 +37,12 @@ function fatorial(a) {
     }
     return fat 
 }
-console.log(fatorial(5))
+console.log(fatorial(fator))
 
-//---------------------------
+console.log('--------------------------------------------------------------------')
+
 //RECURSIVIDADE
+let fatori = parseFloat(readlineSync.question('Qual o numero que deseja descobrir o fatorial?: '))
 function Fatorial(m){
     if(m==1) {
         return 1
@@ -42,4 +50,6 @@ function Fatorial(m){
         return m*Fatorial(m-1)
     }
 }
-console.log(Fatorial(5))
+console.log(Fatorial(fatori))
+
+console.log('--------------------------------------------------------------------')
